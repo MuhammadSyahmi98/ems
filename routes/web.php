c7b72106c7c476c7d520d36fd84ed16f955fd8fa
Muhammad Syahmi <syahmijalil12@gmail.com>
2021-08-12 14:44:51 +0800

    Authetication

--- routes/web.php
+++ routes/web.php
@@ -16,34 +16,41 @@ use App\Http\Controllers\DepartmentController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/department', [DepartmentController::class, 'index'])->name('departments-index');
-Route::get('/department/create', [DepartmentController::class, 'create'])->name('departments-create');
-Route::post('/department/create', [DepartmentController::class, 'store'])->name('departments-store');
-Route::get('/department/{id}', [DepartmentController::class, 'edit'])->name('departments-edit');
-Route::patch('/department/{id}', [DepartmentController::class, 'update'])->name('departments-update');
-Route::delete('/department/{id}', [DepartmentController::class, 'destroy'])->name('departments-destroy');
+Route::group(['middleware'=>'auth'], function(){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
+    Route::get('/department', [DepartmentController::class, 'index'])->name('departments-index');
+    Route::get('/department/create', [DepartmentController::class, 'create'])->name('departments-create');
+    Route::post('/department/create', [DepartmentController::class, 'store'])->name('departments-store');
+    Route::get('/department/{id}', [DepartmentController::class, 'edit'])->name('departments-edit');
+    Route::patch('/department/{id}', [DepartmentController::class, 'update'])->name('departments-update');
+    Route::delete('/department/{id}', [DepartmentController::class, 'destroy'])->name('departments-destroy');
+    
+    
+    Route::get('/role', [RoleController::class, 'index'])->name('roles-index');
+    Route::get('/role/create', [RoleController::class, 'create'])->name('roles-create');
+    Route::post('/role/create', [RoleController::class, 'store'])->name('roles-store');
+    Route::get('/role/{id}', [RoleController::class, 'edit'])->name('roles-edit');
+    Route::patch('/role/{id}', [RoleController::class, 'update'])->name('roles-update');
+    Route::delete('/role/{id}', [RoleController::class, 'destroy'])->name('roles-destroy');
+    
+    Route::get('/user', [UserController::class, 'index'])->name('users-index');
+    Route::get('/user/create', [UserController::class, 'create'])->name('users-create');
+    Route::post('/user/create', [UserController::class, 'store'])->name('users-store');
+    Route::get('/user/{id}', [UserController::class, 'edit'])->name('users-edit');
+    Route::patch('/user/{id}', [UserController::class, 'update'])->name('users-update');
+    Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('users-destroy');
+});
 
 
-Route::get('/role', [RoleController::class, 'index'])->name('roles-index');
-Route::get('/role/create', [RoleController::class, 'create'])->name('roles-create');
-Route::post('/role/create', [RoleController::class, 'store'])->name('roles-store');
-Route::get('/role/{id}', [RoleController::class, 'edit'])->name('roles-edit');
-Route::patch('/role/{id}', [RoleController::class, 'update'])->name('roles-update');
-Route::delete('/role/{id}', [RoleController::class, 'destroy'])->name('roles-destroy');
-
-Route::get('/user', [UserController::class, 'index'])->name('users-index');
-Route::get('/user/create', [UserController::class, 'create'])->name('users-create');
-Route::post('/user/create', [UserController::class, 'store'])->name('users-store');
-Route::get('/user/{id}', [UserController::class, 'edit'])->name('users-edit');
-Route::patch('/user/{id}', [UserController::class, 'update'])->name('users-update');
-Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('users-destroy');
 
 
